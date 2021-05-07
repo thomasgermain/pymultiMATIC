61c256f142d6bdce7e8a0c1d2e574b66cd735879
Thomas Germain <12560542+thomasgermain@users.noreply.github.com>
2021-05-07 17:47:37 +0200

    version (#62)
    Version 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pymultiMATIC',
-      version='4.0.0',
+      version='0.4.0',
       description='Python interface with Vaillant multiMATIC',
       long_description_content_type='text/markdown',
       long_description=long_description,
